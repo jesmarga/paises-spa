020f2b98f5d9d84c2f4616e5cfe0cbba9f52fba3
jesmarga <jesusesteban_mtz@hotmail.com>
2023-05-29 12:08:02 -0600

    Fin de sección 9

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private contriesService: CountriesService) { }
+
+  searchByCapital(term: string): void {
+    this.contriesService.searchCapital(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
+  }
+}
---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styles: [
+  ]
+})
+export class ByCountryPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private contriesService: CountriesService) { }
+
+  searchByCountry(term: string): void {
+    this.contriesService.searchCountry(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
+  }
+
+}
